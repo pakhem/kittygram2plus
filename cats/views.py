d6ae5b5e4f7ad691e648dc58de59d621ec5e22ff
pakhem <pakhem@gmail.com>
2022-09-16 15:12:19 +0300

    123

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,29 @@
 from rest_framework import viewsets
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    throttle_scope = 'low_request'
+    pagination_class = CatsPagination
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
